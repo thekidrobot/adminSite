ada653dbba9491f8f2fa1562008b6b46f1f223c2
Andres Gonzalez <andgonzalez81@gmail.com>
2012-02-01 23:42:21 +0530

    Workaholic

---
+++ webservices/ws_GetChannelList.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+// incluir libreria nusoap
+require_once('conexion.inc.php');
+require_once('lib/nusoap.php');
+
+// Crear server
+$soap_server = new nusoap_server();
+
+$page = $_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+$page = 'http://'.substr($page,0,strrpos($page,"/"));
+
+$ns = $page;
+// configurar WSDL
+$soap_server->configureWSDL('WDSL Authentication', $ns);
+
+// registrar función
+$soap_server->register
+(
+    'receiveUserData',
+    array('userId' => 'xsd:string'),
+    array('return' => 'xsd:string'),
+    $ns,
+    $ns.'#receiveUserData',
+    'rpc',
+    'encoded',
+    'ReceiveUserId'
+);
+
+function receiveUserData($pin='')
+{
+		$arr_msg = array('status'=> '');
+		$user = array();
+		$channels = array();
+		
+		$sql = "SELECT distinct
+						lc.id as id,
+						lc.url as url,
+						lc.number as number,
+						lc.name as name,
+						lc.small_pic as poster,
+						lc.big_pic as posterLarge
+					 FROM
+						livechannels lc,
+						packages_livechannels pl,
+						subscribers sc,
+						subscribers_packages sp
+					 WHERE
+						pl.resource_id = lc.id AND
+						pl.package_id = sp.package_id AND
+						sp.subscriber_id = sc.id AND
+						sc.pin = '$pin'";
+		
+		$result = mysql_query($sql);  
+		if(mysql_num_rows($result) == 0)
+		{
+				$arr_msg['status'] = 'failure';
+				$arr_msg['categories'] = '';				
+		}
+		else
+		{
+				while($row = mysql_fetch_object($result))
+				{
+						array_push
+						($channels,$user['id'] = trim($row->id),$user['url'] = trim($row->url),
+						 $user['number'] = trim($row->number),$user['name'] = trim($row->name),
+						 $user['poster']=trim($row->poster),$user['posterLarge']=trim($row->posterLarge));
+						
+						$usuario.=json_encode($user).',';
+				}
+				$usuario = "[".substr($usuario,0,strlen($usuario)-1)."]";
+
+				$arr_msg['status'] = 'success';
+				$arr_msg['movies'] = $usuario;		
+		}
+		$usuario = str_replace('\\','',json_encode($arr_msg));
+		$usuario = str_replace('"[','[',$usuario);
+		$usuario = str_replace(']"',']',$usuario);
+		return $usuario;
+}
+
+$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+$soap_server->service($HTTP_RAW_POST_DATA);
+
+?> 
